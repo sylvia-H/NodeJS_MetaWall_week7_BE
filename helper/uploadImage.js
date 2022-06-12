a538fadded463dbf74d1cfa2aba65e9411d68850
sylvia-H <sylvie11gaia@gmail.com>
2022-06-12 17:29:45 +0800

    add apiLimiter handler

--- helper/uploadImage.js
+++ helper/uploadImage.js
@@ -7,6 +7,8 @@ const apiLimiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (request, response, next, options) =>
+    response.status(options.statusCode).send(options.message),
 });
 
 const upload = multer({
